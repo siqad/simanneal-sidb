70a6fa50c704f519fd1aab431840bb9a78c099af
Samuel Ng <samuelngsh@gmail.com>
2018-06-30 22:54:51 -0700

    Attempted to reimplement some linear algebra operations on CUDA. Those implementations are currently not thread safe and so are  useless.

--- sim_anneal.cu
+++ sim_anneal.cu
@@ -12,31 +12,72 @@
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+
 //#define STEADY_THREASHOLD 700       //arbitrary value used in restarting
 
 using namespace phys;
 
 std::mutex siqadMutex;
 
-// Test CUDA code
-__global__ void add(int n, float *x, float *y)
-{
-  for (int i = 0; i < n; i++)
-    y[i] = x[i] + y[i];
-}
-
-// System energy
-__global__ void systemEnergy(int n_dbs, float *v, float *n, float *v_ext, float *v_ij)
+// Total system energy including Coulombic repulsion and external voltage.
+// TODO get this working on multiple threads. Not supposed to add to the same variable *v as this isn't thread-safe.
+__global__ void systemEnergy(float *v, int n_dbs, float *n, float *v_ext, float *v_ij)
 {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = blockDim.x * gridDim.x;
 
-  *v=0;
   for (int i=index; i<n_dbs; i+=stride) {
     *v -= v_ext[i] * n[i];
     for (int j=i+1; j<n_dbs; j++)
       *v += v_ij[i*n_dbs + j] * n[i] * n[j];
   }
+}
+
+// Total potential from Coulombic repulsion in the system.
+// TODO get this working on multiple threads. Not supposed to add to the same variable *v as this isn't thread-safe.
+__global__ void totalCoulombPotential(float *v, int n_dbs, float *dn, float *v_ij)
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+
+  float temp;
+  for (int i=index; i<n_dbs; i+=stride) {
+    temp = 0;
+    for (int j=0; j<n_dbs; j++)
+      temp += v_ij[i*n_dbs+j] * dn[j];
+    *v += 0.5 * temp * dn[i];
+  }
+}
+
+// a matrix vector product where the matrix must be square with dimension nxn.
+/*__device__ void matrixVectorProd(int n, float *prod, float *A, float *a)
+{
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      // TODO this initialization is probably wrong, figure out proper way
+      if (i==0 && j==0)
+        *prod[i] = 0;
+      *prod[i] += A[i*n+j] * a[j];
+    }
+  }
+}
+
+// a dot product between two vectors v and u.
+__device__ void dotProd(int n, float *prod, float *a, float *b)
+{
+  for (int i=0; i<n; i++) {
+    // TODO this initialization is probably wrong, figure out proper way
+    if (i==0)
+      *prod = 0;
+    *prod += a[i] * b[i];
+  }
+}*/
+
+__global__ void hopEnergyDelta(float *v_del, int n_dbs, float *v_local, float *v_ij, int i, int j)
+{
+  *v_del = v_local[i] - v_local[j] - v_ij[i*n_dbs+j];
 }
 
 
@@ -75,37 +116,6 @@ void SimAnneal::runSim()
 
   // SIM ANNEAL
   simAnneal();
-
-  /*// CUDA test code
-  int N = 1<<20;
-  float *x, *y;
-
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
-
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
-
-  // Run kernel on 1M elements on the GPU
-  add<<<1, 256>>>(N, x, y);
-
-  // Wait for GPU to finish before accessing on host
-  cudaDeviceSynchronize();
-
-  // Check for errors (all values should be 3.0f)
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++) {
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
-    std::cout << "Max error: " << maxError << std::endl;
-  }
-
-  // Free memory
-  cudaFree(x);
-  cudaFree(y);*/
 }
 
 
@@ -126,6 +136,65 @@ void SimAnneal::simAnneal()
   v_local = v_ext - ublas::prod(v_ij, n);
 
   //steadyPopCount = 0;           //Variable for restarting. Uncomment when restarting.
+
+  /* NOTE cuBLAS attempt, incomplete
+  cudaError_t cuda_stat;
+  cublasStatus_t stat;
+  cublasHandle_t handle;
+  float *dev_n, *dev_v_local, *dev_v_ij, *dev_v_temp;
+
+  cuda_stat = cudaMalloc((void**)&dev_n, n_dbs*sizeof(float));
+  if (cuda_stat != cudaSuccess) {
+    std::cout << "device memory allocation for n failed" << std::endl;
+    return;
+  }
+  cuda_stat = cudaMalloc((void**)&dev_v_ij, n_dbs*n_dbs*sizeof(float));
+  if (cuda_stat != cudaSuccess) {
+    std::cout << "device memory allocation for v_ij failed" << std::endl;
+    return;
+  }
+
+  stat = cublasCreate(&handle);
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "CUBLAS initialization failed" << std::endl;
+    return;
+  }
+
+  stat = cublasSetMatrix(n_dbs, n_dbs, sizeof(*n_arr), n_arr, dev_n, n_dbs);
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "data download failed" << std::endl;
+    cudaFree(dev_n);
+    cublasDestroy(handle);
+    return;
+  }
+  // TODO set vector
+
+  // TODO matrix multiplication
+  float alf=1.0;
+  float beta=1.0;
+  stat = cublasSgemv(handle, CUBLAS_OP_T, n_dbs, n_dbs, &alf, dev_v_ij, n_dbs, dev_n, 1, &beta, v_temp, 1);
+
+  stat = cublasGetMatrix(n_dbs, n_dbs, sizeof(*n_arr), dev_n, n_dbs, n_arr, n_dbs);
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    std::cout << "data upload failed" << std::endl;
+    cudaFree(dev_n);
+    cublasDestroy(handle);
+    return;
+  }
+
+  cudaFree(dev_n);
+  cublasDestroy(handle);
+  */
+  
+  // arrays for CUDA code
+  float *n_arr, *v_ext_arr, *v_ij_arr, *cuda_v, *dn_arr;
+
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&n_arr, n_dbs*sizeof(float));
+  cudaMallocManaged(&v_ext_arr, n_dbs*sizeof(float));
+  cudaMallocManaged(&v_ij_arr, n_dbs*n_dbs*sizeof(float));
+  cudaMallocManaged(&cuda_v, sizeof(float));
+  cudaMallocManaged(&dn_arr, n_dbs*sizeof(float));
 
   // Run simulated annealing for predetermined time steps
   while(t < t_max) {
@@ -154,6 +223,29 @@ void SimAnneal::simAnneal()
       n += dn;
       E_sys += -1 * ublas::inner_prod(v_local, dn) + totalCoulombPotential(dn);
       v_local -= ublas::prod(v_ij, dn);
+
+
+
+      // NOTE CUDA test
+      float c_ver = totalCoulombPotential(dn);
+
+      // copy vector data to array
+      for (int i=0; i<n_dbs; i++) {
+        dn_arr[i] = dn[i];
+        n_arr[i] = n[i];
+        v_ext_arr[i] = v_ext[i];
+        for (int j=0; j<n_dbs; j++) {
+          v_ij_arr[i*n_dbs + j] = v_ij(i,j);
+        }
+      }
+      *cuda_v = 0;
+      ::totalCoulombPotential<<<1, 1>>>(cuda_v, n_dbs, dn_arr, v_ij_arr);
+
+      cudaDeviceSynchronize();
+
+      std::cout << "total coulomb potential c++ : " << c_ver << std::endl;
+      std::cout << "total coulomb potential cuda: " << *cuda_v << std::endl;
+
     }
 
 
@@ -200,15 +292,6 @@ void SimAnneal::simAnneal()
     timeStep();
   }
 
-  // CUDA test code
-  float *n_arr, *v_ext_arr, *v_ij_arr, *cuda_v;
-
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&n_arr, n_dbs*sizeof(float));
-  cudaMallocManaged(&v_ext_arr, n_dbs*sizeof(float));
-  cudaMallocManaged(&v_ij_arr, n_dbs*n_dbs*sizeof(float));
-  cudaMallocManaged(&cuda_v, sizeof(float));
-
   // copy vector data to array
   for (int i=0; i<n_dbs; i++) {
     n_arr[i] = n[i];
@@ -222,7 +305,8 @@ void SimAnneal::simAnneal()
   }
 
   // Run kernel on 1M elements on the GPU
-  ::systemEnergy<<<1, 1>>>(n_dbs, cuda_v, n_arr, v_ext_arr, v_ij_arr);
+  *cuda_v = 0;
+  ::systemEnergy<<<1, 1>>>(cuda_v, n_dbs, n_arr, v_ext_arr, v_ij_arr);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
@@ -235,6 +319,8 @@ void SimAnneal::simAnneal()
   cudaFree(n_arr);
   cudaFree(v_ext_arr);
   cudaFree(v_ij_arr);
+  cudaFree(cuda_v);
+  cudaFree(dn_arr);
 
   writeStore(this, threadId);
 }
