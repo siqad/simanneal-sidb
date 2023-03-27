3ed50f6e59123d6ba50c946166e2d0388f6bd5e0
Samuel Ng <samuelngsh@gmail.com>
2023-03-27 10:06:46 -0700

    chore: remove old commented code

--- setup.py
+++ setup.py
@@ -9,27 +9,3 @@ setup (
         packages = ['pysimanneal'],
         zip_safe = False
         )
-
-##!/usr/bin/env python
-#
-#'''
-#setup.py file for SWIG example
-#'''
-#
-#from distutils.core import setup, Extension
-#import os
-#os.environ["CC"] = "g++"
-#os.environ["CXX"] = "g++"
-#
-#simanneal_module = Extension('_simanneal',
-#                           sources=['simanneal_wrap.cxx', 'simanneal.cc', 
-#                               'global.cc'],
-#                           )
-#
-#setup (name = 'simanneal',
-#       version = '0.1',
-#       author      = 'Samuel Ng',
-#       description = '''Python wrapper for SimAnneal ground state finder.''',
-#       ext_modules = [simanneal_module],
-#       py_modules = [],
-#       )
